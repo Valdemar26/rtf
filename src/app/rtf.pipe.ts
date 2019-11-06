4bf771c9ae8797882f9628f3152f0095c1c3d300
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-11-06 18:12:51 +0200

    added comments

--- src/app/rtf.pipe.ts
+++ src/app/rtf.pipe.ts
@@ -8,11 +8,18 @@ export class RtfToHtmlPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
     console.log('RTF: ', value);
 
-    value.replace(/ansi/, /'TEST'/);
-    const z = value.match(/'#$D#$A'/g || []).length;
+    // value.replace(/ansi/, /'TEST'/);  //  \
+    console.log('SPLIT: ', value.split('/\'#\\$D#\\$A\'/'));
+    console.log(value.match(/'#\$D#\$A'/g || []).length);
 
-    console.log('NEW RTF: ', value, typeof value, value.length, z);
+    console.log('NEW RTF: ', value, typeof value, value.length);
     return value;
   }
+
+  /**
+   *   #$D#$A   - новий рядок
+   *   RED TEXT CAPS BOLD FS14 FF Arial  - текст
+   *
+   */
 
 }
